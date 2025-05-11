96f237ffa5cd5c12e81ecc6f619a3fdf60b73db1
Yargachin-wye <78979106+Yargachin-wye@users.noreply.github.com>
2025-05-12 00:06:56 +0700

    game over trigger

---
+++ Assets/Scripts/Generator/Generators/GeneratorPoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using Managers.Generator.Generators;
+using Managers.Pools;
+using UnityEngine;
+using Random = System.Random;
+
+namespace Generator.Generators
+{
+    public class GeneratorPoint : Managers.Generator.Generator
+    {
+        [SerializeField] private PointsTypes pointsTypes;
+        [SerializeField] private Transform pointPosition;
+
+        public override IEnumerator Init(Random random, Vector2 position)
+        {
+            yield return base.Init(random, position);
+            PointsContainer.Instance.AddPoint(pointsTypes, pointPosition.position);
+            yield return null;
+        }
+    }
+}
